322f972ed607f075bff620b2454e916e9a1972e4
Maglb <magalilebon@Magalis-MBP.lan>
2023-11-25 13:55:59 -0700

    function to add icon according to sky condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var cityEl = $('#search-city');
 var searchBtn = $('#search-btn');
 var todayWeatherEl = $('#today-weather');
 var todayDateEl = $('#today-date');
+var weatherIconEl = $('#icon');
 var latitude;
 var longitude;
 var requestUrl;
@@ -18,124 +19,154 @@ var todayWeather;
 
 var submitLocationSearch = function (event) {
     event.preventDefault();
-  console.log("event triggered");
+    console.log("event triggered");
     var city = cityEl.val().trim();
-  
+
     if (city) {
         getLocation(city);
-        todayDateEl.text(city.toUpperCase() +" " + dayjs().format('DD/MM/YYYY')); 
-    //   it is not woking
-    } else if (getLocation(city)==null){
-      alert('Please enter a city');
+        todayDateEl.text(city.toUpperCase() + " " + dayjs().format('DD/MM/YYYY'));
+        //   it is not woking
+    } else if (getLocation(city) == null) {
+        alert('Please enter a city');
     }
-  };
+};
 
-  searchBtn.on('click', submitLocationSearch);
+searchBtn.on('click', submitLocationSearch);
 
-  // Function to get the latitude and longitude of the search city through the GeoAPI
+// Function to get the latitude and longitude of the search city through the GeoAPI
 function getLocation(city) {
-  // fetch request gets a list of all the repos for the node.js organization
-  requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=a0290a3291b38896066eaae36dc53ecf";
+    // fetch request gets a list of all the repos for the node.js organization
+    requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=a0290a3291b38896066eaae36dc53ecf";
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
 
-    latitude = data[0].lat;
-   longitude = data[0].lon;
-   console.log(latitude);
-   console.log(longitude);
-   getCurrentWeather();
-//    getUpcomingWeather();
-    });
+            latitude = data[0].lat;
+            longitude = data[0].lon;
+            console.log(latitude);
+            console.log(longitude);
+            getCurrentWeather();
+            //    getUpcomingWeather();
+        });
 };
 
 
 
 
 function getCurrentWeather() {
-  // fetch request gets a list of all the repos for the node.js organization
-//   var weatherApi = "lat=" + latitute + "&lon=" + longitude + "&appid={a0290a3291b38896066eaae36dc53ecf";
-// getLocation();
-todayWeather = [];
+    // fetch request gets a list of all the repos for the node.js organization
+    //   var weatherApi = "lat=" + latitute + "&lon=" + longitude + "&appid={a0290a3291b38896066eaae36dc53ecf";
+    // getLocation();
+    todayWeather = [];
 
-requestUrl = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + latitude + "&lon=" + longitude + "&appid=a0290a3291b38896066eaae36dc53ecf";
+    requestUrl = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + latitude + "&lon=" + longitude + "&appid=a0290a3291b38896066eaae36dc53ecf";
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      console.log(typeof data.main.temp);
-      console.log(data.main.humidity);
-      console.log(data.wind.speed);
-      temp = "Temp: " + Math.round(1.8*(data.main.temp - 273) + 32) + " °F";
-      console.log(temp);
-      wind = "Wind: " + Math.round(data.wind.speed * 2.236936) + " MPH";
-      humidity = "Humidity: " + data.main.humidity + " %";
-      console.log(wind);
-      todayWeather = [temp, wind, humidity];
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            console.log(typeof data.main.temp);
+            console.log(data.main.humidity);
+            console.log(data.wind.speed);
+            temp = "Temp: " + Math.round(1.8 * (data.main.temp - 273) + 32) + " °F";
+            console.log(temp);
+            wind = "Wind: " + Math.round(data.wind.speed * 2.236936) + " MPH";
+            humidity = "Humidity: " + data.main.humidity + " %";
+            console.log(wind);
+            todayWeather = [temp, wind, humidity];
+            console.log(data.weather[0].main)
 
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      for (var i = 0; i < todayWeather.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var liElemt = document.createElement('li');
+            if (data.weather[0].main == "Thunderstorm") {
+                weatherIconEl.attr("class","fa-solid fa-bolt");
 
-        // Setting the text of link and the href of the link
-        liElemt.textContent = todayWeather[i];
+            } 
+            
+            else if (data.weather[0].main == "Clouds") {
+                weatherIconEl.attr("class","fa-solid fa-cloud");
 
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        todayWeatherEl.append(liElemt);
-      }
-    });
+            } 
+            
+            else if (data.weather[0].main == "Rain") {
+                weatherIconEl.attr("class","fa-solid fa-cloud-shower-heavy");
+
+            } else if (data.weather[0].main == "Snow") {
+                weatherIconEl.attr("class","fa-solid fa-snowflake");
+            } else if (data.weather[0].main == "Clear") {
+                weatherIconEl.attr("class","fa-solid fa-sun");
+            } else {
+                weatherIconEl.text("");
+            }
+
+            //Loop over the data to generate a table, each table row will have a link to the repo url
+            for (var i = 0; i < todayWeather.length; i++) {
+                // Creating elements, tablerow, tabledata, and anchor
+                var liElemt = document.createElement('li');
+
+                // Setting the text of link and the href of the link
+                liElemt.textContent = todayWeather[i];
+
+                // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                todayWeatherEl.append(liElemt);
+            }
+        });
 };
 
 
 
 function getUpcomingWeather() {
     // fetch request gets a list of all the repos for the node.js organization
-  //   var weatherApi = "lat=" + latitute + "&lon=" + longitude + "&appid={a0290a3291b38896066eaae36dc53ecf";
-  // getLocation();
-  todayWeather = [];
-  
-  requestUrl = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + latitude + "&lon=" + longitude + "&appid=a0290a3291b38896066eaae36dc53ecf";
+    //   var weatherApi = "lat=" + latitute + "&lon=" + longitude + "&appid={a0290a3291b38896066eaae36dc53ecf";
+    // getLocation();
+    todayWeather = [];
+
+    requestUrl = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + latitude + "&lon=" + longitude + "&appid=a0290a3291b38896066eaae36dc53ecf";
 
     fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-    //     console.log(typeof data.main.temp);
-    //     console.log(data.main.humidity);
-    //     console.log(data.wind.speed);
-    //     temp = "Temp: " + Math.round(1.8*(data.main.temp - 273) + 32) + " °F";
-    //     console.log(temp);
-    //     wind = "Wind: " + Math.round(data.wind.speed * 2.236936) + " MPH";
-    //     humidity = "Humidity: " + data.main.humidity + " %";
-    //     console.log(wind);
-    //     todayWeather = [temp, wind, humidity];
-  
-  
-//         //Loop over the data to generate a table, each table row will have a link to the repo url
-//         // for (var i = 1; i < data.list.length; i++) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            //     console.log(typeof data.main.temp);
+            //     console.log(data.main.humidity);
+            //     console.log(data.wind.speed);
+            //     temp = "Temp: " + Math.round(1.8*(data.main.temp - 273) + 32) + " °F";
+            //     console.log(temp);
+            //     wind = "Wind: " + Math.round(data.wind.speed * 2.236936) + " MPH";
+            //     humidity = "Humidity: " + data.main.humidity + " %";
+            //     console.log(wind);
+            //     todayWeather = [temp, wind, humidity];
 
 
-//         //     console.log(data.list[i].main.temp)
-//           // Creating elements, tablerow, tabledata, and anchor
-//           var liElemt = document.createElement('li');
-  
-//     //       // Setting the text of link and the href of the link
-//     //       liElemt.textContent = todayWeather[i];
-  
-//     //       // Appending the link to the tabledata and then appending the tabledata to the tablerow
-//     //       todayWeatherEl.appendChild(liElemt);
-      }
-)}
+            //         //Loop over the data to generate a table, each table row will have a link to the repo url
+            //         // for (var i = 1; i < data.list.length; i++) {
+
+
+            //         //     console.log(data.list[i].main.temp)
+            //           // Creating elements, tablerow, tabledata, and anchor
+            //           var liElemt = document.createElement('li');
+
+            //     //       // Setting the text of link and the href of the link
+            //     //       liElemt.textContent = todayWeather[i];
+
+            //     //       // Appending the link to the tabledata and then appending the tabledata to the tablerow
+            //     //       todayWeatherEl.appendChild(liElemt);
+        }
+        )
+}
 //   )};
 
 
+// // ICONS CODE
+
+// SoleiL: <i class="fa-solid fa-sun"></i>
+// Nuage: <i class="fa-solid fa-sun"></i>
+// Snow: <i class="fa-solid fa-sun"></i>
+// Orage: <i class="fa-solid fa-sun"></i>
+// Rain: <i class="fa-solid fa-sun"></i>
